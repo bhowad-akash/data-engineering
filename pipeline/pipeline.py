bd32cc9ad7e94753f06933fed9577116fe026c82
Akash Bhowad <149307473+bhowad-akash@users.noreply.github.com>
2026-05-31 16:48:30 +0000

    pipeline

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,13 +1,13 @@
 import sys
 import pandas as pd
-print('hello-world')
+print('Hello-World!')
 
 print("arguments", sys.argv)
+
+day = int(sys.argv[1])
+print(f"Running pipeline for day {day}")
 
 df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
 print(df.head())
 
 df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
-
-day = int(sys.argv[1])
-print(f"Running pipeline for day {day}")
