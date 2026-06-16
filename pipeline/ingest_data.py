24095845cabb9dfceb8892b1f46ee25c3369316d
Akash Bhowad <149307473+bhowad-akash@users.noreply.github.com>
2026-06-16 18:33:39 +0000

    pipeline with CLI parameters

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,3 +1,4 @@
+import click
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
@@ -26,25 +27,31 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
-def run():
-    year = 2021
-    month = 1
-
-    target_table = 'yellow_taxi_data'
-
-    pg_username = "root"
-    pg_password = "root"
-    pg_host = "localhost"
-    pg_database = "ny_taxi"
-    pg_port = "5432"
-
-    chunksize = 100000
-
+@click.command()
+@click.option('--year', default=2021, show_default=True, type=int, help='Year of taxi data to ingest')
+@click.option('--month', default=1, show_default=True, type=int, help='Month of taxi data to ingest')
+@click.option('--target-table', default='yellow_taxi_data', show_default=True, help='Destination table name')
+@click.option('--pg-username', default='root', show_default=True, help='PostgreSQL username')
+@click.option('--pg-password', default='root', show_default=True, help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', show_default=True, help='PostgreSQL host')
+@click.option('--pg-database', default='ny_taxi', show_default=True, help='PostgreSQL database name')
+@click.option('--pg-port', default='5432', show_default=True, help='PostgreSQL port')
+@click.option('--chunksize', default=100000, show_default=True, type=int, help='Number of rows per chunk')
+def run(
+    year: int,
+    month: int,
+    target_table: str,
+    pg_username: str,
+    pg_password: str,
+    pg_host: str,
+    pg_database: str,
+    pg_port: str,
+    chunksize: int,
+):
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
     url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
-    
-    engine = create_engine(f'postgresql://{pg_username}:{pg_password}@{pg_host}:{pg_port}/{pg_database}')
 
+    engine = create_engine(f'postgresql://{pg_username}:{pg_password}@{pg_host}:{pg_port}/{pg_database}')
 
     df_iter = pd.read_csv(
         url,
@@ -58,18 +65,18 @@ def run():
     for df_chunk in tqdm(df_iter):
         if first:
             df_chunk.head(n=0).to_sql(
-                name=target_table, 
-                con=engine, 
+                name=target_table,
+                con=engine,
                 if_exists='replace'
             )
             first = False
 
         df_chunk.to_sql(
-            name=target_table, 
-            con=engine, 
+            name=target_table,
+            con=engine,
             if_exists='append'
-            )
-        
+        )
+
 
 if __name__ == '__main__':
     run()
